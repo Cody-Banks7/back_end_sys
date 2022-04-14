bf60e3df0d8f0d9575ebce1455c5cdcc6dfaa1f8
Cody-Banks7 <75161424+Cody-Banks7@users.noreply.github.com>
2022-04-14 21:05:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import os
-# from flask_cors import CORS
+import subprocess
+import re
+
 app = Flask(__name__)
 
 
@@ -11,6 +13,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def test():
     # basic_para = request.values.get('basic_para')
+    global Exestr
     workerNum = request.values.get('workerNum')
     containerNum = request.values.get('containerNum')
     maxThread = request.values.get('maxThread')
@@ -26,11 +29,53 @@ def test():
     print(query_info)
     print(inputQuery)
 
-    str="hive --database ssb_10_orc_transformed -e"+'"source /home/hive/app/queries/ssb_2_transformed/Q1.3.sql;"'
-    ret=os.system(str)
-    print(ret)
-    return "success"
+    app_str = "hive --database " + set_info + " -e \"source /home/hive/queries/ssb/" + query_info + ".sql;\""
+    print(app_str)
+    # ret=os.system(str)
+    ret = subprocess.Popen([app_str], shell=True, stdout=subprocess.PIPE)
+    (out, err) = ret.communicate()
+    print(bytes.decode(out))
 
+    # get yarn
+    app_id = ""
+    for s in bytes.decode(out):
+        if s.startswith("application id:"):
+            app_id = s.strip().split(':')[1]
+    yarn_command = "yarn logs -applicationId application_" + app_id + " > vis4GHive.log"
+    ret = subprocess.Popen([yarn_command], shell=True, stdout=subprocess.PIPE)
+    (out, err) = ret.communicate()
+    print(bytes.decode(out))
+
+    # get vertex
+    task_re = re.compile('.*VertexName: (.*)Vertex.*, TaskAttemptID:(.*), processorName.*')
+    vertex_list = []
+    with open("vis4GHive.log") as f:
+        for line in f:
+            res = task_re.findall(line)
+            if res:
+                vertex_id = res[0][0]
+                if vertex_id not in vertex_list:
+                    vertex_list.append(vertex_id)
+
+    # add into ignore
+    subprocess.Popen(["hadoop fs -cat /tmp/plan/ignore_vertices.txt"], shell=True, stdout=subprocess.PIPE)
+    text = "{\n"
+    for vertex in vertex_list:
+        text += "\""+vertex+"\": false,\n"
+    text += "}"
+    file=open("/tmp/plan/ignore_vertices.txt",mode='w')
+    file.write(text)
+
+    #repalce jar
+    subprocess.Popen(["cp ~/experiment/hive-exec-3.1.0.jar.hive ~/env/hive-3.1.0/lib/hive-exec-3.1.0.jar"], shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen(["cp hive-cli-3.1.0.jar.nowrite"], shell=True, stdout=subprocess.PIPE)
+
+    #run hive
+    ret = subprocess.Popen([app_str], shell=True, stdout=subprocess.PIPE)
+    (out, err) = ret.communicate()
+    print(bytes.decode(out))
+
+    return "success"
 
 
 if __name__ == '__main__':
